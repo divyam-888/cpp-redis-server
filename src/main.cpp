73821a49c38b0cd8ae557a6bcd6a768ca9699e62
Divyam Nagpal <divyam.nagpal.cd.mat23@itbhu.ac.in>
2025-12-04 03:21:53 +0530

    Implemented concurrent clients with detach

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,22 @@
 #include <chrono>
 using namespace std;
 
-void handleClient(int server_fd) {
-  char buffer[1024];
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
+void handleClient(int client_fd) {
+    char buffer[1024];
+    
+    // Loop to keep talking to THIS client until they disconnect
+    while (true) {
+        ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
+        if (bytes_read <= 0) {
+            std::cout << "Client disconnected\n";
+            break; // Stop the loop if they hang up
+        }
 
-  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-  std::cout << "Client connected\n";
-  ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
-
-  const char *response = "+PONG\r\n";
-  send(client_fd, response, strlen(response), 0);
-  close(client_fd);
+        const char *response = "+PONG\r\n";
+        send(client_fd, response, strlen(response), 0);
+    }
+    
+    close(client_fd); // Hang up the phone
 }
 
 
@@ -75,30 +78,33 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  // struct sockaddr_in client_addr;
-  // int client_addr_len = sizeof(client_addr);
-  // std::cout << "Waiting for a client to connect...\n";
-
   std::cout << "Logs from your program will appear here!\n";
+  std::cout << "Server is listening...\n";
 
-  // int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-  // std::cout << "Client connected\n";
+    while(true) {
+        // 1. Prepare to accept a new connection
+        struct sockaddr_in client_addr;
+        int client_addr_len = sizeof(client_addr);
+        
+        // 2. The Main Thread blocks here WAITING for a connection
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+        
+        if (client_fd < 0) {
+             std::cerr << "Failed to accept\n";
+             break; 
+        }
 
-  // char buffer[1024];
-  // const char *response = "+PONG\r\n";
+        std::cout << "New Client Connected! Spawning thread...\n";
 
-  // while(true) {
-  //   ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
-  //   if(bytes_read <= 0) {
-  //     std::cout << "Done reading\n";
-  //     break;
-  //   }
-  //   send(client_fd, response, strlen(response), 0);
-  // }
+        // 3. Create a thread specifically for THIS client_fd
+        // We use std::thread and pass the function + arguments
+        std::thread client_thread(handleClient, client_fd);
 
-  // close(client_fd);
-  thread t1(handleClient, server_fd);
-  thread t2(handleClient, server_fd);
+        // 4. DETACH the thread
+        // This is crucial. It tells C++: "Let this thread run on its own in the background. 
+        // I (Main) am going back to the top of the loop to wait for the next person."
+        client_thread.detach(); 
+    }
 
   close(server_fd);
   return 0;
