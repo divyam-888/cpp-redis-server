0370097a9970b50d93c1d18592903544b8d2551b
Divyam Nagpal <divyam.nagpal.cd.mat23@itbhu.ac.in>
2025-12-04 02:25:16 +0530

    [Multiple Pings]

--- src/main.cpp
+++ src/main.cpp
@@ -7,59 +7,76 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
+
+  // Socket is used to create TCP server to accept inbound connections. It is like a telephone to accept incoming calls
+  // Socket returns the file descriptor of the TCP server. It has parameters: Domain, Type, Protocol
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+  if (server_fd < 0)
+  {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+  {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
+  /* Before socket/server can accept connections we need to bind it to an address. It's like setting up our 
+  telephone on the wall and letting everyone know where to dial */
+
+  // To bind a socket to a port, we need to set up a sockaddr_in structure with the address family, IP address, and port
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-  
+
+  // Now our socket/server is ready to take connections. We need to pass socket descriptor and backlog i.e, maximum # of connections our server can take to listen()
+  // Backlog = 5 means If 5 people call at once, put 4 on hold. If a 6th calls, drop them.
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
+  if (listen(server_fd, connection_backlog) != 0)
+  {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
   std::cout << "Waiting for a client to connect...\n";
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment the code below to pass the first stage
-   
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
-  
-  // char buffer[1024];
-  // ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
 
-  char* response = "+PONG\r\n";
-  send(client_fd, response, strlen(response), 0);
+  char buffer[1024];
+  const char *response = "+PONG\r\n";
+
+  while(true) {
+    ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
+    if(bytes_read <= 0) {
+      std::cout << "Done reading\n";
+      break;
+    }
+    send(client_fd, response, strlen(response), 0);
+  }
 
   close(client_fd);
   close(server_fd);
